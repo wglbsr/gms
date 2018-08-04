703cc733af8ede7d44683f85e2e7e325bda4a699
wglbs <wglbsr@163.com>
2018-08-05 01:21:59 +0800

    Initial commit

---
+++ src/main/java/com/dyny/gms/GmsApplication.java
@@ -0,0 +1,12 @@
+package com.dyny.gms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmsApplication.class, args);
+	}
+}
