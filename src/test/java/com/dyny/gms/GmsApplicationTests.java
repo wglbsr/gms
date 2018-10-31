bd6f12ca336127c594200727b1113304b1bcb493
wglbs <wglbsr@163.com>
2018-10-31 14:03:49 +0800

    update

--- src/test/java/com/dyny/gms/GmsApplicationTests.java
+++ src/test/java/com/dyny/gms/GmsApplicationTests.java
@@ -1,16 +1,7 @@
 package com.dyny.gms;
 
-import org.apache.log4j.Logger;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.ValueOperations;
-import org.springframework.test.context.junit4.SpringRunner;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class GmsApplicationTests {
 //    private static Logger logger = Logger.getLogger(GmsApplicationTests.class);
 //
